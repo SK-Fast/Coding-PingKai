aed965994890ce128e593104eb2edbfcb3e164fd
BATjuiceXD <pacharamumu@gmail.com>
2022-09-16 16:57:50 +0700

    World2

--- src/pages/lesson-select/index.vue
+++ src/pages/lesson-select/index.vue
@@ -1,3 +1,24 @@
 <template>
-    sfrg
+    <h1>World 2</h1>
+    <div class="container">
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+        <img src="@/assets/Group 62.png" alt="sorry"/>
+    </div>
 </template>
+<style scoped>
+.container {
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+grid-template-rows: 1fr 1fr; 
+column-gap: 10px;
+row-gap: 50px;
+}
+</style>
