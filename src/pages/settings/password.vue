6e615bb5cd24ecc682d0018b4c72c469413f7878
BATjuiceXD <pacharamumu@gmail.com>
2022-10-16 08:50:24 +0700

    Password_Red

--- src/pages/settings/password.vue
+++ src/pages/settings/password.vue
@@ -1,13 +1,12 @@
 <template>
 <h1 class="con col-2 ">รหัสผ่าน</h1>
-<input class="w-25 form-control gg col-2" placeholder="รหัสผ่านเก่า" />
-<input class="w-25 form-control gg col-2" placeholder="รหัสผ่านใหม่" />
-<input class="w-25 form-control gg col-2" placeholder="ยืนยันรหัสผ่านใหม่" />
+<input class="w-25 form-control  col-2" placeholder="รหัสผ่านเก่า" />
+<input class="w-25 form-control  col-2" placeholder="รหัสผ่านใหม่" />
+<input class="w-25 form-control  col-2" placeholder="ยืนยันรหัสผ่านใหม่" />
 <button class="btn btn-primary w-12.5 mt-2">Continue</button>
 </template>  
 <style scoped>
 .con {
-    background-color: cyan;
     color: red;
 }
 .gg {
