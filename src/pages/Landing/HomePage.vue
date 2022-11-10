f6c06df348fb1c9b35d521eb8c2187ed4832d1a6
DevPixels <60780448+SK-Fast@users.noreply.github.com>
2022-11-10 07:59:58 +0000

    Landing Rewrite

--- src/pages/Landing/HomePage.vue
+++ src/pages/Landing/HomePage.vue
@@ -2,30 +2,38 @@
 
 </script>
 <template>
-<div class="con">
-<h1>HomePage</h1>
-<img class="con" src="@/assets/logo/logoracist.png" alt="sorry">
-</div>
-<div class="cat">
-  <img src="@/assets/Cat.png" alt="haha">
-  <h1>Good WEb APp</h1>  
-</div>
+  <div class="landing-banner-container w-100">
+    <div class="bg-primary w-100 h-100 landing-banner">
+      <div class="container text-white">
+        <img src="@/assets/logo/logo-white.svg" height="100">
+        <div class="ms-2">
+          <h4>Code Like a pro, I use LInuX bTW</h4>
+          <div class="col col-2 mt-4">
+            <button class="btn btn-light-200">มาเริ่มกันเลย! <vue-feather type="arrow-right" stroke="#000" size="16px" /></button>
+            <a class="ms-3 text-white">Login</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="landing-waves"></div>
+    
+  </div>
 </template>
 <style scoped>
-.con {
-    display: flex;
-    background: rgb(190,33,33);
-    background: linear-gradient(0deg, rgba(190,33,33,1) 0%, rgba(211,84,37,1) 33%, rgba(253,187,45,1) 100%);
-    width: 50%;
-    height: auto;
-
+.landing-banner-container {
+  height: 350px;
 }
-.cat {
-    display: flex;
-    justify-content: space-evenly;
-    background: rgb(190,33,33);
-    background: linear-gradient(0deg, rgba(190,33,33,1) 0%, rgba(211,84,37,1) 33%, rgba(253,187,45,1) 100%);
-    width: 50%;
-    height: auto;
+
+.landing-waves {
+  width: 100%;
+  height: 65px;
+  background-image: url(@/assets/landing/landing-wave.svg);
+  background-size: contain;
+  background-position: center;
+}
+
+.landing-banner {
+  display: flex;
+  align-items: center;
 }
 </style>
