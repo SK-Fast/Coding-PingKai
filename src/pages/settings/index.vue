47ea0f54c2165ee9d210c0f4d5e3adb6f1cf9585
DevPixels <60780448+SK-Fast@users.noreply.github.com>
2022-11-15 06:54:44 +0000

    light theme update, incomp

--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -1,77 +1,24 @@
 <template>
-<div class="container">
-    <h2 class="col-5 margin">การตั้งค่า</h2>   
-    <button class="som" @click="() =>this.$router.push('/profile')">
-        ข้อมูลผู้ใช้
-    </button>
-    <button class="con" @click="() =>this.$router.push('/password')">
-       รหัสผ่าน
-    </button>
-    <button class="con" @click="() =>this.$router.push('/Pog')">
-        การตั้งค่า
-    </button>
-    <button class="com" @click="">ลงชื่อออก</button>
-</div> 
+    <div class="container">
+        <div class="row col-2">
+            <h2 class="ms-0 mb-2">การตั้งค่า</h2>   
+            <button class="btn btn-mid-transparent settings-btn" @click="() =>this.$router.push('/profile')">
+                ข้อมูลผู้ใช้
+            </button>
+            <button class="btn settings-btn" @click="() =>this.$router.push('/password')">
+               รหัสผ่าน
+            </button>
+            <button class="btn settings-btn" @click="() =>this.$router.push('/Pog')">
+                การตั้งค่า
+            </button>
+
+            <button class="btn btn-outline-danger settings-btn" @click="">ลงชื่อออก</button>
+        </div>
+    </div> 
 </template>
 <style scoped>
-.som {
-display: flex;
-flex-direction: row;
-align-items: center;
-padding: 10px 100px;
-gap: 10px;
-
-
-border: none;
-background: #FF7733;
-
-box-shadow: inset 0px -1px 0px rgba(21, 21, 21, 0.25), inset 0px 1px 0px rgba(255, 255, 255, 0.25);
-border-radius: 10px;
-flex: none;
-order: 0;
-flex-grow: 0;
-}
-.con {
-    display: flex;
-flex-direction: row;
-align-items: center;
-padding: 10px 100px;
-gap: 10px;
-
-border: none;
-background: rgba(0, 0, 0, 0.1);
-
-box-shadow: inset 0px -1px 0px rgba(21, 21, 21, 0.25), inset 0px 1px 0px rgba(255, 255, 255, 0.25);
-border-radius: 10px;
-
-/* Inside auto layout */
-
-flex: none;
-order: 1;
-flex-grow: 0;
-}
-.com {
-    display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 10px 100px;
-gap: 10px;
-
-
-color: #F23051;
-
-border: 2px solid #F23051;
-
-box-shadow: inset 0px -1px 0px rgba(21, 21, 21, 0.25), inset 0px 1px 0px rgba(255, 255, 255, 0.25);
-border-radius: 10px;
-
-
-flex: none;
-order: 3;
-flex-grow: 0;
-}
-.margin {
-    margin-right: 500px;
+.settings-btn {
+    margin-bottom: 5px;
+    text-align: left;
 }
 </style>
