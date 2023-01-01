5fdc485bc95853759502702298cc91c37fb69918
DevPixels <thummajil25580@gmail.com>
2023-01-01 23:10:17 +0700

    Automate Developer Links

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,22 @@
 <script setup>
   import Navbar from "@/components/Navbar.vue"
+  import { ref } from "vue";
+  import links from "./router.js"
+
+  const devMode = ref(import.meta.env.MODE == 'development')
 </script>
 
 <template>
   <div>
     <Navbar />
-    <nav>
-      <router-link to="/">Landing</router-link>
-       | 
-      <router-link to="/World1">World1</router-link>
-      |
-      <router-link to="/World2">World2</router-link>
-      |
-      <router-link to="/settings">settings</router-link>
-      |
-      <router-link to="/HomePage">HomePage</router-link>
-      |
-      <routerLink to="/profile">profile</routerLink>
-      |
-      <routerLink to="/password">password</routerLink>
-      |
-      <routerLink to="/exit">exit</routerLink>
-      |
-      <RouterLink to="Pog">Pog</RouterLink>
-      |
-      <RouterLink to="/workspace">workspace</RouterLink>
-      |
-      <RouterLink to="/dashboard">dashboard</RouterLink>
-    </nav><br>
+    <div v-if="devMode" class="d-flex align-items-center p-2 bg-light-300 mb-2">
+      <nav class="d-flex flex-grow-1">
+        <p v-for="(item, index) in links" class="m-0 me-1">
+          <router-link :to="item.path">{{ item.path }}</router-link>
+        </p>
+      </nav>
+      <code class="text-end me-2">Developer Menu</code>
+    </div>
 
     <router-view v-slot="{ Component }">
       <transition name="fade">
