71d7bc8b7f12cc2b3474aefc522c337a62f747bb
DevPixels <60780448+SK-Fast@users.noreply.github.com>
2023-01-12 05:34:33 +0000

    Jam

---
+++ src/pages/congrat/congrat.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+<template>
+<div class="C">
+    
+</div>
+<div class="C">
+   <h1>ยินดีด้วย</h1>
+</div>
+</template>
+<style scoped>
+.C{
+    width: 50%;
+}
+</style>
--- src/pages/settings/password.vue
+++ src/pages/settings/password.vue
@@ -42,4 +42,4 @@ onMounted(async () => {
     }
 
 })
-</script>
+</script> 
