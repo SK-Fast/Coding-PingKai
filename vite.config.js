007173da3c42220a6fb9c17bbb5c9616d5f26955
SK-Fast <thummajil25580@gmail.com>
2023-04-06 20:09:03 +0700

    [Fix] Login bug #3

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
     },
     workbox: {
       navigateFallback: '/index.html',
-      navigateFallbackWhitelist: [/^(?!\/__)/]
+      navigateFallbackAllowlist: [/^(?!\/__)/]
     }
   })],
   resolve: {
